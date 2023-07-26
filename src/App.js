2643c18135629dd7312d3881437a7073c99d6e6c
Kilian Medina <kilianmedina@Kilians-MacBook-Air.local>
2023-07-26 16:42:03 -0600

    texto invertido

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+const App = () => {
+  const [textoOriginal, setTextoOriginal] = useState('')
+  const [textoInvertido, setTextoInvertido] = useState('')
+
+  const textoAction = (event) => {
+    setTextoOriginal(event.target.value)
+  }
+
+  const clickAction = () => {
+    const invertir = textoOriginal.split('').reverse().join('')
+    setTextoInvertido(invertir)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Invertir texto</h1>
+      <input
+        type="text"
+        placeholder="Ingresar texto"
+        value={textoOriginal}
+        onChange={textoAction}
+      />
+      <button onClick={clickAction}>Invertir</button>
+      <h2>Texto invertido</h2>
+      <input
+        type="text"
+        readOnly
+        value={textoInvertido}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
